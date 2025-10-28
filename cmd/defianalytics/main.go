d5a346c2e37849d004639fc20d1ebd4a5046d7a4
Zhu <zhusonglai@Live.cn>
2025-10-28 22:28:37 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/defianalytics/main.go
@@ -0,0 +1,23 @@
+// cmd/defianalytics/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "defianalytics/internal/defianalytics"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := defianalytics.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
